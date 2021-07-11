686c8ff03367a5e663648cbd2ff615c370d4862c
YukkiMiraiLaptop <thanh.putdut@gmail.com>
2021-07-11 16:01:37 +0700

    section 6

---
+++ 03-fontend/angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+  name!: String;
+  unitPrice!: number;
+  sku!: string;
+  description!: string;
+  imageUrl!: string;
+  active!: string;
+  unitsInStock!: number;
+  dateCreated!: Date;
+  lastUpdated!: Date;
+}
